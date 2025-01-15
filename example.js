d5a26dd707bace11180adc418e227ad05a1592fc
Zeke Sikelianos <zeke@sikelianos.com>
2025-01-15 10:54:45 -0800

    finish stats

--- example.js
+++ example.js
@@ -6,6 +6,12 @@ const mostRunModels = chain(models)
   .take(10)
   .value()
 
+console.log("Most run models:")
 for (const model of mostRunModels) {
   console.log(model.url)
 }
+
+import stats from './stats.mjs'
+
+console.log("Stats for Flux Schnell:")
+console.log(stats["black-forest-labs/flux-schnell"].slice(-5))
